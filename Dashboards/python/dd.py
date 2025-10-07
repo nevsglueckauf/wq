ba459e9d8e2679b9cced4f509dc19e5ca0cd051c
Sven Schrodt <sven@schrodt.club>
2025-10-07 10:25:05 +0200

    Neue Verzeichnisstruktur & Änderungen

--- Dashboards/python/dd.py
+++ Dashboards/python/dd.py
@@ -5,16 +5,16 @@
 class DD:
 
     pages = {
-         "Sessions": "Sitzungsanalyse",
-         "Sales": "Umsätze n. Vertriebskan.",
-         "Upload" : "Dateiverwaltung",
-         "Products": "Produkte",
-         "Test": "Lab page"
+        "Sessions": "Sitzungsanalyse",
+        "Sales": "Umsätze n. Vertriebskan.",
+        "Upload": "Dateiverwaltung",
+        "Products": "Produkte",
+        "Test": "Lab page",
     }
 
     col_transl = {
         "sess_ctry": "Land",
-        "sess_loc": "Ort",  
+        "sess_loc": "Ort",
         "datum": "Datum",
         "sug_platform": "Empfehlungsplattform",
         "landing_uri": "URI Landing Page",
@@ -30,6 +30,32 @@ class DD:
 
     session_y = ["jump_offs", "sess_cart_add", "sess_co", "sess_co_done", "sessions"]
 
-    sales_chan_y = ["Vertriebskanal","Bestellungen","Bruttoumsatz","Rabatte","Rückgaben","Nettoumsatz","Versandgebühren","Steuern","Gesamtumsatz"]
+    sales_chan_y = [
+        "Vertriebskanal",
+        "Bestellungen",
+        "Bruttoumsatz",
+        "Rabatte",
+        "Rückgaben",
+        "Nettoumsatz",
+        "Versandgebühren",
+        "Steuern",
+        "Gesamtumsatz",
+    ]
+
+    sess_trans_orig = {
+        "sess_ctry": "Sitzungsland",
+        "sess_loc": "Sitzungsort",
+        "date": "Monat",
+        "sug_platform": "Empfehlungsplattform",
+        "landing_uri": "Landing-Page-URL",
+        "session_duration": "Durchschnittliche Sitzungsdauer",
+        "pages_per_sess": "Seitenaufrufe pro Sitzung",
+        "jump_offs": "Absprünge",
+        "sess_cart_add": "Sitzungen, in denen Artikel zum Warenkorb hinzugefügt wurden",
+        "sess_co": "Sitzungen, bei denen der Checkout erreicht wurde",
+        "sess_co_done": "Sitzungen, bei denen der Checkout abgeschlossen wurde",
+        "sessions": "Sitzungen",
+    }
+
 
 # cloc --by-file --fullpath --match-d 'dash'
